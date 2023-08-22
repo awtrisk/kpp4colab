7ff881f7afb56eb6757f643a511d60f74539a8de
veryamazinglystupid <unlawfulspoon864@gmail.com>
2023-08-22 17:50:29 +0530

    add makething

---
+++ maker.py
@@ -0,0 +1,8 @@
+import subprocess
+
+subprocess.run(["git", "clone", "https://github.com/LostRuins/koboldcpp.git"])
+
+print("Repository cloned successfulily!")
+
+subprocess.run(["cd", "koboldcpp"]
+subprocess.run(["make", "LLAMA_CUBLAS=1"]
