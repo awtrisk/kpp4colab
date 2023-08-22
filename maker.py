15620a369ea56a876644d90a596a393086554090
veryamazinglystupid <unlawfulspoon864@gmail.com>
2023-08-22 18:13:23 +0530

    mfw termux dev experience

--- maker.py
+++ maker.py
@@ -1,7 +1,7 @@
 import subprocess
 import os
 
-os.system("git clone https://github.com/LostRuins/koboldcpp.git
+os.system("git clone https://github.com/LostRuins/koboldcpp.git")
 print("cloned")
 os.system("cd koboldcpp")
 os.system("make LLAMA_CUBLAS=1")
