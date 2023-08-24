d5a355efa61136634a2c0be57e96df09341f6e52
veryamazinglystupid <unlawfulspoon864@gmail.com>
2023-08-24 14:03:38 +0530

    fix1

--- maker.py
+++ maker.py
@@ -1,7 +1,5 @@
-import subprocess
 import os
 
 os.system("git clone https://github.com/LostRuins/koboldcpp.git")
-print("cloned")
-os.system("cd koboldcpp")
+os.chdir("koboldcpp")
 os.system("make LLAMA_CUBLAS=1")
